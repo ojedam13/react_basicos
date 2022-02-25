b073e534f98c083210fac7fa121f365142d37b34
ojedam13 <m_ojeda17@hotmail.com>
2022-02-25 18:01:21 -0300

    primer componente y basicos de jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <Header />
 
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    
+
+    return (
+        <h1 className="encabezado">Tienda virtual</h1>   
+
+    )
+}
+
+export default Header;
