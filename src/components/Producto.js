832c18e4e2fc48f2556a5b1f30e2922eab48dd67
ojedam13 <m_ojeda17@hotmail.com>
2022-02-27 20:53:47 -0300

    eventos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const Producto = ({producto}) => {
     
-    const { nombre, precio, id } = producto
+    const { nombre, precio, id } = producto;
+
+    //Agregar producto al carrito
+    const seleccionarProducto = id => {
+        console.log('comprando... ', id);
+    } 
     
     return (   
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button
+                type="button"
+                onClick={()=> seleccionarProducto(id)}
+            >Comprar</button>
 
         </div>
     
