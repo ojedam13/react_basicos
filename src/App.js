fa169bec42a432f685c42c39e3f1fdab0e284531
ojedam13 <m_ojeda17@hotmail.com>
2022-02-27 20:46:16 -0300

    useState mostrar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
 
 function App() {
+  
+  //Crear listado de productos
+  const [productos, setProductos ] = useState([
+    { id : 1, nombre: 'Camisa React JS', precio: 50},
+    { id : 2, nombre: 'Camisa vue JS', precio: 40},
+    { id : 3, nombre: 'Camisa node JS', precio: 30},
+    { id : 4, nombre: 'Camisa Angular JS', precio: 20}
+  ]);
 
+  
   // obetener la fecha
     const fecha = new Date().getFullYear();
     
@@ -13,6 +23,13 @@ function App() {
         titulo='Tienda virtual'
         numero= {20}
       />
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto 
+          key = {producto.id}
+          producto = {producto}
+        />
+      ))}
 
       <Footer 
         fecha = {fecha}
---
+++ src/components/Producto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Producto = ({producto}) => {
+    
+    const { nombre, precio, id } = producto
+    
+    return (   
+        <div>
+            <h2>{nombre}</h2>
+            <p>${precio}</p>
+
+        </div>
+    
+    );
+}
+ 
+export default Producto;
